06042e4f1d117514abe4f95dd5d01f1268d090d4
jrbeals <jrbeals20@gmail.com>
2026-03-09 13:55:50 -0600

    code

--- gui.py
+++ gui.py
@@ -2,31 +2,33 @@ from tkinter import *
 from tkinter import ttk
 
 def addStudent(first,last):
-    print("SQL add to database {first} {last}")
-
-def addButton(newText, newRow, newCommand=None):
-    if newCommand==None:
-        newCommand=lambda :ourPrint(newText)
-    ttk.Button(frm, text=newText, command=newCommand).grid(column=0,row=newRow,sticky="EW")
+    print(f"SQL Add to database {first} {last}")
 
 def ourPrint(text):
     print(text)
 
+def addButton(newText,newRow,newCommand=None):
+    if newCommand==None:
+        newCommand=lambda :ourPrint(newText)
+    ttk.Button(frm, text=newText, command=newCommand).grid(column=0, row=newRow,sticky="EW")
+
 root = Tk()
-frm= ttk.Frame(root,padding=10)
+frm = ttk.Frame(root, padding=10)
 frm.grid()
+ttk.Label(frm, text="First Name").grid(column=0, row=0,sticky="EW")
 
-ttk.Label(frm, text="First Name").grid(column=0,row=0,sticky="EW")
-firstName = ttk.Entry(form).grid(column=0,row=0,sticky="EW")
+first=StringVar()
+last=StringVar()
 
-ttk.Label(frm, text="Last Name").grid(column=0,row=0,sticky="EW")
-firstName = ttk.Entry(form).grid(column=1,row=1,sticky="EW")
+ttk.Entry(frm,textvariable=first).grid(column=1, row=0,sticky="EW")
+ttk.Label(frm, text="Last Name").grid(column=0, row=1,sticky="EW")
+ttk.Entry(frm,textvariable=last).grid(column=1, row=1,sticky="EW")
 
-addButton("Add a student",2,lambda : addStudent(first.get(),last.get()))
-addButton("List all students",3)
-addButton("Update student",4)
-addButton("Student registrations",5)
-addButton("Delete a student",6)
+addButton("Add a Student",2,lambda :addStudent(first.get(),last.get()))
+addButton("List all Students",3)
+addButton("Update a student",4)
+addButton("Registrations for a student",5)
+addButton("Delete a Student",6)
 addButton("Quit",7,root.destroy)
 
 root.mainloop()
