c40326378cba41b4926217c07d17bc5078cdd480
jrbeals <jrbeals20@gmail.com>
2026-04-22 09:54:42 -0600

    River Data

---
+++ riverData/doImport.sql
@@ -0,0 +1,26 @@
+/* DO $$
+DECLARE
+    item TEXT;
+    my_list TEXT[] := ARRAY['cond', 'discharge'];
+BEGIN
+    FOREACH item IN ARRAY my_list
+    LOOP */
+        create table disO (
+            id text,
+            time_series_id text,
+            monitoring_location_id text,
+            parameter_code int,
+            statistic_id int,
+            time text,
+            value float,
+            unit_of_measure text,
+            approval_status text,
+            qualifier text,
+            last_modified text
+        );
+
+        \COPY disO FROM do_primary-time-series.csv (HEADER, FORMAT CSV );
+/*
+    END LOOP;
+END $$;
+*/
